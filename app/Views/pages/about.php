c40ed2d01dde6c20da02218c1b27ceb3263d9812
nurarifin256 <arifinnur402@gmail.com>
2021-02-25 21:51:33 +0700

    Tambah file pages ( controller dan view )

---
+++ app/Views/pages/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt rem repellendus nulla omnis veniam illum nam doloribus recusandae quis fugit tempore natus mollitia, vero esse, minus enim vel distinctio fuga.</p>
+</body>
+
+</html>
